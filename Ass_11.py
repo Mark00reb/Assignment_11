a4772efa597a606cb1ab36f68fecc7960cefda2c
Mark00reb <u17135983@tuks.co.za>
2019-05-16 14:29:45 +0200

    'final'

--- Ass_11.py
+++ Ass_11.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as graph
 import pandas as pand 
 dataMpg = pand.read_csv('Z:\Documents\_INF 354\Assignment 11\Assignment_11\mpg.csv')
 
@@ -9,17 +9,17 @@ dataMpg ['Displacement_Per_Sec'] = dataMpg['displacement']/dataMpg['acceleration
 light = dataMpg['weight'] < 3300
 
 
-
 #Correlation
 dataMpg['Cor_Co_Effecient'] = dataMpg['weight'].corr(dataMpg['horsepower'])
 
 print(dataMpg[light])
 
-#Plot
-plot.plot(dataMpg['weight'], dataMpg['mpg'], 'ro')
-plot.title('How weight influences the Mile Per Gallon')
-plot.xlabel('weight')
-plot.ylabel('MPG')
+#Plot graph
+graph.bar(dataMpg['weight'], dataMpg['mpg'], color='purple', width= 125)
+graph.title('How weight influences the Mile Per Gallon')
+graph.xlabel('weight(lbs)')
+graph.ylabel('MPG')
+#graph.plot(dataMpg['weight'], dataMpg['mpg']- dataMpg['weight'], color='orange')
 
-#Display
-plot.show()
+#Display graph
+graph.show()
